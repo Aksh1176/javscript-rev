f727f9e823351e0406fcc34e2f83f6218f2ccee0
Akshat <70104707+Aksh1176@users.noreply.github.com>
2024-10-29 17:38:07 +0000

    data types discussion

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version.
+
+// alert(3 + 3); //we are using node.js not browser.
+
+// number => 2 to pwer 53
+// bigInt
+// string = ""
+// boolean => true/false
+// null => standalone value (representation of an empty value)
+// undefined
+// symbol => unique
+
+
+//object
+console.log(typeof "Akshat"); // string
+console.log(typeof null); // object
+console.log(typeof undefined);
+
