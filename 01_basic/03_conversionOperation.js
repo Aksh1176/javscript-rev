49d7f1da0612144416bacd877647c8a3190aa16c
Akshat <70104707+Aksh1176@users.noreply.github.com>
2024-10-29 18:11:53 +0000

    conversion discussion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -4,8 +4,31 @@ let score3 = Number(undefined); //NaN
 let score4 = Number(true);      //1
 let score5 = Number(false);     //0
 let score6 = Number({});        //NaN
-console.table([score1,score2,score3,score4,score5,score6])
+//console.table([score1,score2,score3,score4,score5,score6])
 //console.table([typeof score1, typeof score2, typeof score3, typeof score4, typeof score5,typeof score6]);
 
 console.log("33" , 33)
+
+/* **************************************Operations********************* */
+
+
+let str1 = "Akshat";
+let str2 = " Choudhary";
+let finalString = str1 + str2;
+console.log(finalString);
+
+let covertNumber = +true; // 1 (Conerted to number by using +)
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
 
