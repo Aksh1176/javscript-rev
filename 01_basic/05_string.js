20a453c2d56324d2b461f2313d9f7ec13b1452e4
Akshat <70104707+Aksh1176@users.noreply.github.com>
2024-11-04 16:31:08 +0000

    string discussions

---
+++ 01_basic/05_string.js
@@ -0,0 +1,14 @@
+const name = "Akshat";
+let age = 20;
+
+console.log(`Hello my name is ${name} and age is ${age}`);
+const gameName = new String("Badshah");
+const userDetail = "   Akshat      "
+console.log(userDetail.trim())
+
+//Go through all string functions.
+
+//ref mdn docs.
+
+
+
